cf0833eaffab0a796266c1f7fe8dda34f376c2ff
firasayari10 <ayari.firas@esprit.tn>
2025-09-02 22:03:17 +0100

    front end : data displau , filtering , row selection and checkbox implementations

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,57 @@
+"use client" ;
+
+import { Card , CardContent , CardHeader , CardTitle} from "@/components/ui/card"
+import {Button} from "@/components/ui/button"
+import { useNewAccount } from "@/features/hooks/use-new-accounts";
+import { DataTable } from "@/components/ui/data-table";
+
+// to be removed later because i'm just experimienting with mock data for the sake of testing the front end 
+import  { Payment,columns}  from "./columns"
+
+
+
+const data:Payment[] = [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "125893",
+      amount: 1,
+      status: "success",
+      email: "a@example.com",
+    }
+    // ...
+  ]
+
+
+
+const AccountPage = () => {
+
+
+    const newAccount = useNewAccount();
+    return (
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 mb-24 ">
+            <Card className=" border-none drop-shadow-sm">
+            <CardHeader className=" gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                <CardTitle className="text-xl line-clamp-1" >
+                    Accounts page
+                </CardTitle>
+                <Button size="sm" onClick={newAccount.onOpen}>
+                    Add new 
+                </Button>
+     
+            </CardHeader>
+            <CardContent>
+                <DataTable filterKey="email" columns={columns} data={data} />
+            </CardContent>
+            </Card>
+            
+        </div>
+    );
+
+};
+
+export default AccountPage ;
