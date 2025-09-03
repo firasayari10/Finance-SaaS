2cb5574b708f65073f3e4b615d72489f602821ad
firasayari10 <ayari.firas@esprit.tn>
2025-09-03 22:36:21 +0100

    bug fix : fixed the delete in bulk button , it now deletes only selected account

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -80,7 +80,7 @@ export function DataTable<TData, TValue>({
           variant="outline" 
           className="ml-auto fon-normal text-xs"
           onClick={() => {
-            onDelete(table.getFilteredRowModel().rows)
+            onDelete(table.getFilteredSelectedRowModel().rows)
             table.resetRowSelection();
           }}>
             <Trash className=" size-4 mr-2"/>
